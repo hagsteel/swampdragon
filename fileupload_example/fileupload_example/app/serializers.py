38cd46384d790f5f4c33956b819a0d98a57809bc
Jonas Hagstedt <hagstedt@gmail.com>
2014-07-16 23:08:24 +0200

    Removed file upload example

--- fileupload_example/fileupload_example/app/serializers.py
+++
@@ -1,7 +0,0 @@
-from swampdragon.serializers.django_model_serializer import DjangoModelSerializer
-
-
-class WithFileSerializer(DjangoModelSerializer):
-    model = 'app.WithFile'
-    publish_fields = ['name', 'file', 'created', 'a_bool']
-    update_fields = ['name', 'file', 'a_bool']
