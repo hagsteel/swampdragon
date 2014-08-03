e1ec3b1aa464461eadd1a4a1c5321ebc62ff5e8d
Jonas Hagstedt <hagstedt@gmail.com>
2014-08-03 22:35:01 +0200

    updated serializers in file upload example

--- fileupload_example/fileupload_example/app/serializers.py
+++ fileupload_example/fileupload_example/app/serializers.py
@@ -1,20 +1,23 @@
-from swampdragon.serializers.django_model_serializer import DjangoModelSerializer
+from swampdragon.serializers.model_serializer import ModelSerializer
 
 
-class WithFileSerializer(DjangoModelSerializer):
-    model = 'app.WithFile'
-    publish_fields = ['name', 'file', 'created']
-    update_fields = ['name', 'file']
+class WithFileSerializer(ModelSerializer):
+    class Meta:
+        model = 'app.WithFile'
+        publish_fields = ('name', 'file', 'created')
+        update_fields = ('name', 'file')
 
 
-class FileSerializer(DjangoModelSerializer):
-    model = 'app.MultiFileModel'
-    publish_fields = ['file']
-    update_fields = ['file']
+class FileSerializer(ModelSerializer):
+    class Meta:
+        model = 'app.MultiFileModel'
+        publish_fields = ('file')
+        update_fields = ('file')
 
 
-class MultiFileSerializer(DjangoModelSerializer):
-    model = 'app.MultiFileModel'
-    publish_fields = ['text', 'files']
-    update_fields = ['text', 'files']
-    files_serializer = FileSerializer
+class MultiFileSerializer(ModelSerializer):
+    class Meta:
+        model = 'app.MultiFileModel'
+        publish_fields = ('text', 'files')
+        update_fields = ('text', 'files')
+        files_serializer = FileSerializer
