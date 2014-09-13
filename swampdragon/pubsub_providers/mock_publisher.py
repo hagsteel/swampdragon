29f22fe484cf0c2fc337eb5974209ec2208bd2f1
Jonas Hagstedt <hagstedt@gmail.com>
2014-09-13 22:30:58 +0200

    Added subscribe and unsubscribe to the mock publisher

--- swampdragon/pubsub_providers/mock_publisher.py
+++ swampdragon/pubsub_providers/mock_publisher.py
@@ -23,3 +23,23 @@ class MockPublisher(object):
 
     def get_channels(self, base_channel):
         return self._get_channels_from_subscriptions(base_channel)
+
+    def subscribe(self, channels, subscriber):
+        for c in channels:
+            if c not in subscribers.keys():
+                subscribers[c] = []
+            subscribers[c].append(subscriber)
+
+    def unsubscribe(self, channels, subscriber):
+        if not isinstance(channels, list):
+            return self.unsubscribe([channels], subscriber)
+        for channel in channels:
+            subscribers[channel].remove(subscriber)
+
+        empty_channels = [k for (k, v) in subscribers.items() if not v]
+        for k in empty_channels:
+            del subscribers[k]
+
+    def remove_subscriber(self, subscriber):
+        channels = [c for c in subscribers if subscriber in subscribers[c]]
+        self.unsubscribe(channels, subscriber)
