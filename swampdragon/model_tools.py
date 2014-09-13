07c4318a45bdc2bbd0aeeb449c8b096dc6e36305
Jonas Hagstedt <hagstedt@gmail.com>
2014-09-13 22:31:16 +0200

    Removed unused code and tidy up

--- swampdragon/model_tools.py
+++ swampdragon/model_tools.py
@@ -11,10 +11,7 @@ def get_property(obj, field):
             if attr is None:
                 return None
         return attr
-    try:
-        return getattr(obj, field, None)
-    except:
-        return None
+    return getattr(obj, field, None)
 
 
 def string_to_list(val):
@@ -25,34 +22,3 @@ def get_model(model):
     if isinstance(model, str):
         return get_django_model(*model.split('.', 1))
     return model
-
-
-def get_m2m_model_from_field_name(instance, field_name):
-    try:
-        return getattr(instance, field_name).model
-    except:
-        return None
-
-
-def get_field_from_m2m_model_by_model(instance, model):
-    """
-    Provided an instance and a model, find the
-    field name on the instance, where it has an m2m relationship
-    to the model
-    """
-    for f, ignore in instance._meta.get_all_related_m2m_objects_with_model():
-        if f.model == model:
-            return f.get_accessor_name()
-
-    for f in instance._meta.get_all_field_names():
-        try:
-            field = instance._meta.get_field_by_name(f)[0]
-            if hasattr(field, 'get_accessor_name'):
-                name = field.get_accessor_name()
-            else:
-                name = field.name
-            if get_m2m_model_from_field_name(instance, name):
-                return name
-        except:
-            continue
-    return None
