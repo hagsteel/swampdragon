5232a7f7d878e7bc69aebfc95d690040e8239845
Jonas Hagstedt <hagstedt@gmail.com>
2014-09-16 00:09:39 +0200

    Tidy up settings provider

--- swampdragon/settings_provider.py
+++ swampdragon/settings_provider.py
@@ -3,14 +3,13 @@ from swampdragon.default_settings import SwampDragonSettings
 
 
 class SettingsHandler(RequestHandler):
-
     def set_default_headers(self):
         self.set_header("Content-Type", "text/javascript")
 
     def get(self, *args, **kwargs):
         data = '''window.swampdragon_settings = {settings};
 window.swamp_dargon_host = "{protocol}://{host}";
-        '''.format(**{
+'''.format(**{
             'settings': SwampDragonSettings().to_dict(),
             'host': self.request.headers['Host'],
             'protocol': self.request.protocol,
