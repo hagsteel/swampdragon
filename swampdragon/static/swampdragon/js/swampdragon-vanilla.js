ef3017d2ca53892e77047fea0aeb6e8a2af5e078
Jonas Hagstedt <hagstedt@gmail.com>
2014-11-09 01:20:42 +0100

    Vanilla JS

---
+++ swampdragon/static/swampdragon/js/swampdragon-vanilla.js
@@ -0,0 +1,76 @@
+var VanillaDragon = function(options) {
+    options = options || {};
+    var endpoint = window.swampdragon_settings.endpoint;
+    var sdInstance = new SwampDragon(options);
+    var _dragon = {
+        _callbackId: 0,
+        _readyCallbacks: [],
+        _channelMessageCallbacks: [],
+        _loginRequiredCallbacks: [],
+        _heartbeatCallbacks: [],
+
+        _getCallbackName: function () {
+            var callbackName = 'cb_' + this._callbackId;
+            this._callbackId++;
+            if (this._callbackId > 9999)
+                this._callbackId = 0;
+            return callbackName;
+        },
+
+        connect: function() {
+            sdInstance.connect(window.swampdragon_host, endpoint)
+        },
+
+        callRouter: function (verb, route, args, success, failure, channel) {
+            var _this = this;
+            var callbackName = this._getCallbackName();
+            sdInstance.on(callbackName, function (context, data) {
+                if (context.state == 'success') {
+                    if (success) { success(context, data); }
+                }
+                else if (context.state == 'error') {
+                    if (failure) { failure(context, data); }
+                } else {
+                    _this._handleError(context, data, deferred);
+                }
+            });
+            sdInstance.callRouter(verb, route, args, callbackName, channel);
+        },
+
+        getSingle: function (route, data, success, failure) {
+            this.callRouter('get_single', route, data, success, failure, null);
+        },
+
+        getList: function (route, data, success, failure) {
+            this.callRouter('get_list', route, data, success, failure, null)
+        },
+
+        getPagedList: function (route, data, page, success, failure) {
+            page = page || 1;
+            data['_page'] = page;
+            this.callRouter('get_list', route, data, success, failure, null)
+        },
+
+        "create": function (route, data, success, failure) {
+            this.callRouter('create', route, data, success, failure, null)
+        },
+
+        update: function (route, data, success, failure) {
+            this.callRouter('update', route, data, success, failure, null)
+        },
+
+        "delete": function (route, data, success, failure) {
+            this.callRouter('delete', route, data, success, failure, null)
+        },
+
+        subscribe: function (route, channel, args, success, failure) {
+            this.callRouter('subscribe', route, args, success, failure, channel);
+        },
+
+        unsubscribe: function (route, channel, args, success, failure) {
+            this.callRouter('unsubscribe', route, args, success, failure, channel);
+        }
+    };
+    _dragon.connect();
+    return _dragon;
+};
