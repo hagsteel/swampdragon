16c16d86da6f8942fd7b6983cb45f8f1ce29685f
Jonas Hagstedt <hagstedt@gmail.com>
2015-02-01 10:36:45 +0100

    Added custom template tag

---
+++ swampdragon/templatetags/swampdragon_tags.py
@@ -0,0 +1,10 @@
+from django import template
+from django.conf import settings
+
+register = template.Library()
+
+
+@register.simple_tag()
+def swampdragon_settings():
+    root_url = getattr(settings, 'DRAGON_URL') or 'http://localhost:9999/'
+    return '<script type="text/javascript" src="{}settings.js"></script>'.format(root_url)
