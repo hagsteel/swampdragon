fc4c1233beb2a0f4db6bd136dd15fb52f3eb86cf
Jonas Hagstedt <hagstedt@gmail.com>
2015-06-13 22:56:11 +0200

    Updated test that checks for deletions

--- tests/test_selfpublish_delete_on_update.py
+++ tests/test_selfpublish_delete_on_update.py
@@ -13,10 +13,10 @@ class TestSelfPubModel(DragonTestCase):
     def test_self_pub_model(self):
         connection = TestConnection()
 
-        foo_a = FooSelfPub.objects.create(name='test a')
-        foo_b = FooSelfPub.objects.create(name='test b')
+        foo_a = FooSelfPub.objects.create(name='test a', number=123)
+        foo_b = FooSelfPub.objects.create(name='test b', number=456)
         router = FooModelRouter(connection)
-        router.subscribe(channel='testchan', id=foo_a.id)
+        router.subscribe(channel='testchan', number=foo_a.number)
         foo_a.name = 'updated'
         foo_a.save()
         foo_b.name = 'updated'
@@ -24,3 +24,9 @@ class TestSelfPubModel(DragonTestCase):
 
         pd = connection.published_data
         self.assertEqual(len(pd), 1)
+
+        #  Change the number, thus the model no longer match the filter
+        #  this should trigger a delete message
+        foo_a.number = 1
+        foo_a.save()
+        self.assertEqual(connection.last_pub['action'], 'deleted')
