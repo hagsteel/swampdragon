f59d20e155b726c4c8e76d35c70cf1ca738c07e2
Jonas Hagstedt <hagstedt@gmail.com>
2015-06-13 22:42:26 +0200

    Update the way DELETEs are published

--- swampdragon/pubsub_providers/model_publisher.py
+++ swampdragon/pubsub_providers/model_publisher.py
@@ -1,5 +1,5 @@
 from ..pubsub_providers.base_provider import PUBACTIONS
-from ..pubsub_providers.model_channel_builder import filter_channels_by_model
+from ..pubsub_providers.model_channel_builder import filter_channels_by_model, filter_channels_by_dict
 from ..pubsub_providers.publisher_factory import get_publisher
 
 
@@ -13,7 +13,8 @@ def publish_model(model_instance, serializer, action, changed_fields=None):
     base_channel = serializer.get_base_channel()
     all_model_channels = publisher.get_channels(base_channel)
     channels = filter_channels_by_model(all_model_channels, model_instance)
-    remove_from_channels = set(all_model_channels) - set(channels)
+    previous_channels = filter_channels_by_dict(all_model_channels, model_instance._pre_save_state)
+    remove_from_channels = set(previous_channels) - set(channels)
 
     if channels:
         publish_data = dict({'data': serializer.serialize(fields=changed_fields)})
@@ -23,7 +24,7 @@ def publish_model(model_instance, serializer, action, changed_fields=None):
             publish_data['channel'] = c
             publisher.publish(c, publish_data)
 
-    if changed_fields:
+    if changed_fields and remove_from_channels:
         publish_data = {'data': {'id': model_instance.pk}}
         publish_data['action'] = PUBACTIONS.deleted
         for channel in remove_from_channels:
