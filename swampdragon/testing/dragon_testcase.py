97e85488fdf8001d94069badbc8fbda4204bb476
Jonas Hagstedt <hagstedt@gmail.com>
2015-06-27 01:06:40 +0200

    Allow specifying port for async test cases

--- swampdragon/testing/dragon_testcase.py
+++ swampdragon/testing/dragon_testcase.py
@@ -1,4 +1,5 @@
 from tornado.testing import AsyncHTTPTestCase
+from tornado import netutil
 from swampdragon import discover_routes
 from swampdragon import route_handler
 from swampdragon.pubsub_providers.subscriber_factory import get_subscription_provider
@@ -9,9 +10,16 @@ from django.utils.importlib import import_module
 from sockjs.tornado import SockJSRouter
 from tornado import web
 from swampdragon.settings_provider import SettingsHandler
+import socket
 
 
 pub_sub = get_subscription_provider()
+
+
+def _bind_port(port=9988):
+    [sock] = netutil.bind_sockets(port, 'localhost', family=socket.AF_INET)
+    port = sock.getsockname()[1]
+    return sock, port
 
 
 class DragonTestCase(TestCase):
@@ -58,10 +66,23 @@ class DragonTestCaseAsync(AsyncHTTPTestCase):
         self.urls = discover_routes()
         self.app = self._load_app()
 
+    def setUp(self):
+        super(AsyncHTTPTestCase, self).setUp()
+        sock, port = _bind_port(getattr(self, 'port', 9988))
+        self._port = port
+
+        self.http_client = self.get_http_client()
+        self._app = self.get_app()
+        self.http_server = self.get_http_server()
+        self.http_server.add_sockets([sock])
+
     def tearDown(self):
         route_handler.registered_handlers = {}
         pub_sub._channels = []
         pub_sub._subscribers = {}
+
+    def get_http_port(self):
+        return self._port
 
     def _load_app(self):
         routers = []
