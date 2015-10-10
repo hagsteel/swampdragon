a4e5da1d99819dd5271b6403242717dbb0408354
Jacob Kahn <jacobkahn1@gmail.com>
2015-10-10 01:44:49 -0400

    Add optional Redis DB password parameter to settings

--- swampdragon/pubsub_providers/redis_settings.py
+++ swampdragon/pubsub_providers/redis_settings.py
@@ -3,6 +3,7 @@ from django.conf import settings
 redis_host = None
 redis_port = None
 redis_db = None
+redis_password = None
 
 
 def get_redis_host():
@@ -24,3 +25,10 @@ def get_redis_db():
     if not redis_db:
         redis_db = getattr(settings, 'SWAMP_DRAGON_REDIS_DB', 0)
     return redis_db
+
+
+def get_redis_password():
+    global redis_password
+    if not redis_password:
+        redis_password = getattr(settings, 'SWAMP_DRAGON_REDIS_PASSWORD', None)
+    return redis_password
